bff77db49f555f07f0fb60ed15a2673c8399dcf1
Justin Berkshire <justinwendellberkshire@gmail.com>
2022-08-18 11:10:10 -0500

    functionally ready, need to package into class

--- main.cpp
+++ main.cpp
@@ -20,21 +20,34 @@ int main() {
 
     gpio_set_dir(LED, GPIO_OUT);
 
-    uint leads[4] = {0,1,2,3};
+    uint leads[4] = { 2,3,4,5 };
 
-    for(int i = 0; i < 4;i++) {
+    for (int i = 0; i < 4;i++) {
         gpio_init(leads[i]);
         gpio_set_dir(leads[i], GPIO_OUT);
+        // gpio_set_function(leads[i], GPIO_FUNC_GPCK);
+        gpio_put(leads[i], 0);
     };
+
+    gpio_put(LED, 1);
+
+    gpio_put(leads[0], 1);
+    gpio_put(leads[1], 0);
+    sleep_ms(20);
+    gpio_put(leads[2], 1);
+    gpio_put(leads[3], 0);
+    sleep_ms(20);
 
     while (true) {
         gpio_put(leads[0], 1);
         gpio_put(leads[1], 0);
+        sleep_ms(20);
         gpio_put(leads[2], 1);
         gpio_put(leads[3], 0);
         sleep_ms(20);
         gpio_put(leads[0], 0);
         gpio_put(leads[1], 1);
+        sleep_ms(20);
         gpio_put(leads[2], 0);
         gpio_put(leads[3], 1);
         sleep_ms(20);

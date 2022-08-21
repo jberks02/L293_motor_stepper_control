17c37d8125336c9ca49e67b7318c0be34427f724
Justin Berkshire <justinwendellberkshire@gmail.com>
2022-08-21 11:41:49 -0500

    beginning work on stepper class encapsulation for L293D IC

--- main.cpp
+++ main.cpp
@@ -10,6 +10,42 @@
 #include "main.h"
 using namespace std;
 
+class L293D_STEPPER {
+private: int reductionFactor;
+private: uint coilOne[2];
+private: uint coilTwo[2];
+public: L293D_STEPPER(uint firstCoil[2], uint secondCoil[2], uint gearReduction[2]) {
+    for (int i = 0;i < 2;i++) {
+        gpio_init(firstCoil[i]);
+        gpio_set_dir(firstCoil[i], GPIO_OUT);
+        gpio_put(firstCoil[i], 0);
+    }
+    for (int i = 0;i < 2;i++) {
+        gpio_init(secondCoil[i]);
+        gpio_set_dir(secondCoil[i], GPIO_OUT);
+        gpio_put(secondCoil[i], 0);
+    }
+
+    reductionFactor = gearReduction[1] / gearReduction[2];
+}
+private: void setCoilDirection(char direction, uint coil[2]) {
+    if (direction != 'r' && direction != 'f') return;//invalid direction may throw in future;
+    if (coil != coilOne && coil != coilTwo) return; // invalid coil set throw event;
+
+    uint coilSequence[2];
+
+    if (direction == 'f') {
+        coilSequence[0] = coil[0];
+        coilSequence[1] = coil[1];
+    }
+    if (direction == 'r') {
+        coilSequence[0] = coil[1];
+        coilSequence[1] = coil[0];
+    }
+
+}
+};
+
 int main() {
 
     stdio_init_all();
@@ -41,16 +77,16 @@ int main() {
     while (true) {
         gpio_put(leads[0], 1);
         gpio_put(leads[1], 0);
-        sleep_ms(20);
+        sleep_ms(2);
         gpio_put(leads[2], 1);
         gpio_put(leads[3], 0);
-        sleep_ms(20);
+        sleep_ms(2);
         gpio_put(leads[0], 0);
         gpio_put(leads[1], 1);
-        sleep_ms(20);
+        sleep_ms(2);
         gpio_put(leads[2], 0);
         gpio_put(leads[3], 1);
-        sleep_ms(20);
+        sleep_ms(2);
     }
 
     return 0;
